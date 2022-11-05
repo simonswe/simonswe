875f69662d7b44b546b13bac5946181a659834a7
xRoyalPhantom <s355huan@uwaterloo.ca>
2022-11-04 18:42:48 -0700

    Add gh-pages; modification to About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,6 @@ const About = () => {
             <svg name='about' className="svg_curve_top" xmlns="http://www.w3.org/2000/svg" fill="#011F44" viewBox="0 0 100 100" preserveAspectRatio="none" >
                 <path d="M0,100 C20,-80 26,90 100,100" />
             </svg>
-
             <div className="main_container">
                 <div className="left">
                     <img src={ProfilePic} alt="" className="image" />
