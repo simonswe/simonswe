4beca87ad6d456e06dd5ab4e9b7b7a70836b0abd
xRoyalPhantom <s355huan@uwaterloo.ca>
2022-11-06 23:29:25 -0800

    refactor + clean css for satellite.scss

--- src/components/Satellite.jsx
+++ src/components/Satellite.jsx
@@ -11,17 +11,6 @@ const Satellite = () => {
         {//<Parallax className='satellite__background' bgImage={SatelliteImage} strength={500}></Parallax>
         }
             <div className='satellite__container'>
-                <div className='carousel__content'>
-                    <div className="carousel__content__container">
-                        <p className="carousel__content__container__text">I'm a</p>
-                        <ul className="carousel__content__container__list">
-                            <li className="carousel__content__container__list__item">Software Engineer</li>
-                            <li className="carousel__content__container__list__item">Violin Virtuoso</li>
-                            <li className="carousel__content__container__list__item">Passionate Gamer</li>
-                            <li className="carousel__content__container__list__item">Food Enthusiast</li>
-                        </ul>
-                    </div>
-                </div>
                 <div className='typewriter__content'>
                     <span className='img-txt'>
                         <TypeWriter
@@ -39,6 +28,17 @@ const Satellite = () => {
                             }}
                         />
                     </span>
+                </div>
+                <div className='carousel__content'>
+                    <div className="carousel__content__container">
+                        <p className="carousel__content__container__text">I'm a</p>
+                        <ul className="carousel__content__container__list">
+                            <li className="carousel__content__container__list__item">Software Engineer</li>
+                            <li className="carousel__content__container__list__item">Violin Virtuoso</li>
+                            <li className="carousel__content__container__list__item">Passionate Gamer</li>
+                            <li className="carousel__content__container__list__item">Food Enthusiast</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
